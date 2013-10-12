0225215f9ae6826876b9749f874cb279018acd3b
Coleman McCormick <cmccormick@gmail.com>
2013-10-12 19:07:23 -0400

    More changes to sounding converter

--- spinnaker.rb
+++ spinnaker.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'json'
-require 'gdal-ruby/ogr'
 require 'thor'
+require 'active_support/all'
+require 'rgeo/geo_json'
 
 class Spinnaker < Thor
 
@@ -12,10 +13,33 @@ class Spinnaker < Thor
     `ogrinfo -so -al #{options[:file]} | grep "Layer name"`
   end
 
-  desc "soundings", "Extract and convert sounding data"
-  method_option :file, aliases: "-f", desc: "File to list"
+  desc "soundings", "Extract and convert sounding data from S-57"
+  method_option :directory, aliases: "-d", desc: "Directory of raw S-57 data files"
+  method_option :path, aliases: "-p", desc: "Path to save data"
   def soundings
-    `ogr2ogr -f GeoJSON #{options[:file].downcase}_soundings.json #{options[:file]} SOUNDG`
+    Dir.glob(options[:directory] + '/**/*.000') do |file|
+      section = File.basename(file, ".000")
+      `ogr2ogr -f GeoJSON #{options[:path]}/#{section}_soundings_raw.geojson #{file} SOUNDG`
+      puts "Converted #{section}."
+
+      collection = RGeo::GeoJSON.decode(File.read("#{options[:path]}/#{section}_soundings_raw.geojson"), :json_parser => :json)
+
+      geojson = {}
+
+      collection.each do |f|
+        %w"FFTP_RIND EXPSOU NOBJNM OBJNAM QUASOU SOUACC TECSOU VERDAT STATUS INFORM NINFOM NTXTDS SCAMAX TXTDSC REDCAT RECIND".each do |k|
+          f.delete(k)
+        end
+        geojson << RGeo::GeoJSON.encode(f)
+      end
+
+      File.open("#{options[:path]}/#{section}_soundings.geojson","w") do |f|
+        f.write(geojson.to_json)
+        puts "Saved #{section}_soundings.geojson."
+        # File.delete("#{options[:path]}/#{section}_soundings_raw.geojson")
+      end
+
+    end
   end
 
 end
